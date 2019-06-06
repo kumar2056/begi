666d33e5ad90d92d82a73099286b029e036b2955
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2019-06-06 13:49:56 +0530

    Create minushrs.py

---
+++ minushrs.py
@@ -0,0 +1,18 @@
+a,b=input().split()
+k,l=input().split()
+c=int(a)
+d=int(b)
+e=int(k)
+f=int(l)
+if(c>e):
+  g=c-e
+  print(g,end=" ")
+else:
+  h=e-c
+  print(h,end=" ")
+if(d>f):
+  m=d-f
+  print(m)
+else:
+  n=f-d
+  print(n)
