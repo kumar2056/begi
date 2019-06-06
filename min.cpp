7aaa35da7d31c6baafd3ae301731da34adea268b
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2019-06-06 14:45:52 +0530

    Create min.cpp

---
+++ min.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+int a[10000],h,m;
+cin>>p;
+for(int i=0;i<p;i++)
+{
+
+    cin>>a[i];
+}
+for(int i=0;i<p;i++)
+{
+    if(a[0]<=a[i+1])
+    {
+        m=a[0];
+        a[0]=a[i+1];
+        a[i+1]=m;
+    }
+}
+
+
+cout<<" "<<a[0];
+return 0;
+}
